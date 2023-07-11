acce1412206f820632bc61bc05dcf95142d6ec44
RayVentura <you@example.com>
2023-07-11 16:02:38 -0400

    renaming editing flow to editing engine

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Automating video and short content creation with AI'
+LONG_DESCRIPTION = 'A powerful tool for automating content creation. It simplifies video creation, footage sourcing, voiceover synthesis, and editing tasks.'
+
+# Setting up
+setup(
+    name="shortgpt",
+    version=VERSION,
+    author="RayVentura",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'ffmpeg', 
+        'python-dotenv', 
+        'gradio',
+        'openai', 
+        'tiktoken',
+        'tinydb',
+        'tinymongo',
+        'proglog',
+        'yt-dlp',
+        'torch', 
+        'torchaudio',
+        'protobuf==3.20.0',
+        'langchain',
+        'moviepy',
+        'termcolor',
+        'progress',
+        'questionary',
+    ],
+    keywords=['python', 'video', 'content creation', 'AI', 'automation', 'editing', 'voiceover synthesis', 'video captions', 'asset sourcing', 'tinyDB'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
