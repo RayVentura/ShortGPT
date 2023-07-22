2709add91a37535e9e383328500cd8be2bd0d75e
jarrettdev <jarrettdevereaux909@gmail.com>
2023-07-21 21:31:49 -0700

    GPT-4 facts + linting.

--- shortGPT/gpt/facts_gpt.py
+++ shortGPT/gpt/facts_gpt.py
@@ -1,10 +1,13 @@
 from shortGPT.gpt import gpt_utils
 import json
+
+
 def generateFacts(facts_type):
     chat, system = gpt_utils.load_yaml_prompt('shortGPT/prompt_templates/facts_generator.yaml')
     chat = chat.replace("<<FACTS_TYPE>>", facts_type)
-    result = gpt_utils.gpt3Turbo_completion(chat_prompt=chat, system=system, temp=1.3)
+    result = gpt_utils.gpt3Turbo_completion(chat_prompt=chat, system=system, temp=1.3, model='gpt-4-0613')
     return result
+
 
 def generateFactSubjects(n):
     out = []
@@ -13,11 +16,10 @@ def generateFactSubjects(n):
     count = 0
     while len(out) != n:
         result = gpt_utils.gpt3Turbo_completion(chat_prompt=chat, system=system, temp=1.69)
-        count+=1
+        count += 1
         try:
             out = json.loads(result.replace("'", '"'))
         except Exception as e:
             print(f"INFO - Failed generating {n} fact subjects after {count} trials", e)
             pass
-        
     return out
--- shortGPT/gpt/gpt_utils.py
+++ shortGPT/gpt/gpt_utils.py
@@ -1,28 +1,31 @@
 import openai
 import yaml
 import os
-from time import time,sleep
+from time import time, sleep
 import re
 import tiktoken
 import json
 from shortGPT.config.api_db import get_api_key
 
+
 def num_tokens_from_messages(texts, model="gpt-3.5-turbo-0301"):
+    models_to_process = ["gpt-3.5-turbo-0301", "gpt-4-0613"]
     """Returns the number of tokens used by a list of messages."""
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
         encoding = tiktoken.get_encoding("cl100k_base")
-    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
+    if model in models_to_process:  # note: future models may deviate from this
         if isinstance(texts, str):
             texts = [texts]
         score = 0
         for text in texts:
-            score+= 4 + len(encoding.encode(text))
+            score += 4 + len(encoding.encode(text))
         return score
     else:
         raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
         See https://github.com/openai/openai-python/blob/main/chatml.md for information""")
+
 
 def extract_biggest_json(string):
     json_regex = r"\{(?:[^{}]|(?R))*\}"
@@ -31,6 +34,7 @@ def extract_biggest_json(string):
         return max(json_objects, key=len)
     return None
 
+
 def get_first_number(string):
     pattern = r'\b(0|[1-9]|10)\b'
     match = re.search(pattern, string)
@@ -38,22 +42,28 @@ def get_first_number(string):
         return int(match.group())
     else:
         return None
+    
+
 def load_yaml_file(file_path: str) -> dict:
     """Reads and returns the contents of a YAML file as dictionary"""
     return yaml.safe_load(open_file(file_path))
    
+
 def load_json_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         json_data = json.load(f)
     return json_data
 
+
 def load_yaml_prompt(file_path):
-    json_template= load_yaml_file(file_path)
+    json_template = load_yaml_file(file_path)
     return json_template['chat_prompt'], json_template['system_prompt']
+
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
+
 
 def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the answer to anything", temp=0.7, model="gpt-3.5-turbo",max_tokens=1000, remove_nl=True, conversation=None):
     openai.api_key = get_api_key("OPENAI")
@@ -62,11 +72,11 @@ def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the
     while True:
         try:
             if conversation:
-                messages= conversation
+                messages = conversation
             else:
                 messages = [
-                {"role": "system", "content": system},
-                {"role": "user", "content": chat_prompt}
+                    {"role": "system", "content": system},
+                    {"role": "user", "content": chat_prompt}
                 ]
             response = openai.ChatCompletion.create(
                 model=model,
@@ -88,5 +98,4 @@ def gpt3Turbo_completion(chat_prompt="", system="You are an AI that can give the
                 raise Exception("GPT3 error: %s" % oops)
             print('Error communicating with OpenAI:', oops)
             sleep(1)
-
             
