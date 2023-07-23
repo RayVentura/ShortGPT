cfffe7ef8905f5edaf1615ddde6d758b70b5151a
RayVentura <you@example.com>
2023-07-23 19:52:10 -0400

    fix youtube urls not working

--- shortGPT/config/path_utils.py
+++ shortGPT/config/path_utils.py
@@ -2,6 +2,8 @@ import os
 import platform
 import sys
 import subprocess
+import subprocess
+import tempfile
 def search_program(program_name):
     try: 
         search_cmd = "where" if platform.system() == "Windows" else "which"
@@ -22,13 +24,13 @@ import os
 def is_running_in_colab():
     return 'COLAB_GPU' in os.environ
 
-def handle_path(path):
+def handle_path(path, extension = ".mp4"):
     if 'https' in path:
         if is_running_in_colab():
-            import requests
-            import tempfile
-            temp_file = tempfile.NamedTemporaryFile(delete=False)
-            temp_file.write(requests.get(path).content)
+            temp_file = tempfile.NamedTemporaryFile(suffix= extension, delete=False)
+            # The '-y' option overwrites the output file if it already exists.
+            command = ['ffmpeg', '-y', '-i', path, temp_file.name]
+            subprocess.run(command, check=True)
             temp_file.close()
             return temp_file.name
     return path
