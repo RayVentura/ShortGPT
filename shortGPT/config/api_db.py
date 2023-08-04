e25051a3d5c89099649c5f4b355b75ad0893d595
RayVentura <you@example.com>
2023-08-04 16:21:46 -0400

    adding ApiProvider enum

--- shortGPT/config/api_db.py
+++ shortGPT/config/api_db.py
@@ -1,13 +1,23 @@
+import enum
 from shortGPT.database.db_document import TinyMongoDocument
+
+class ApiProvider(enum.Enum):
+    OPENAI = "OPENAI"
+    ELEVEN_LABS = "ELEVEN LABS"
+    PEXELS = "PEXELS"
 
 
 class ApiKeyManager:
     api_key_doc_manager = TinyMongoDocument("api_db", "api_keys", "key_doc", create=True)
 
     @classmethod
-    def get_api_key(cls, name):
-        return cls.api_key_doc_manager._get(name) or ""
+    def get_api_key(cls, key: str or ApiProvider):
+        if isinstance(key, ApiProvider):
+            key = key.value
+        return cls.api_key_doc_manager._get(key) or ""
 
     @classmethod
-    def set_api_key(cls, name, value):
-        return cls.api_key_doc_manager._save({name: value})
+    def set_api_key(cls, key: str or ApiProvider, value: str):
+        if isinstance(key, ApiProvider):
+            key = key.value
+        return cls.api_key_doc_manager._save({key: value})
