fa2d1df23b85cb269a02c704641ba6c11fad21dc
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2025-02-24 00:13:19 -0700

    captions are shown one at a time

--- shortGPT/editing_utils/captions.py
+++ shortGPT/editing_utils/captions.py
@@ -47,10 +47,9 @@ def splitWordsBySize(words, maxCaptionSize):
         captions.append(caption)
     return captions
 
-def getCaptionsWithTime(transcriptions, maxCaptionSize=15, considerPunctuation=True):
+#Single word captions
+def getCaptionsWithTime(transcriptions):
     time_splits = []
-    current_caption = []
-    current_length = 0
     
     # Ensure we only work with transcriptions that have word-level timing
     segments = [seg for seg in transcriptions['segments'] if 'words' in seg]
@@ -60,48 +59,13 @@ def getCaptionsWithTime(transcriptions, maxCaptionSize=15, considerPunctuation=T
     for segment in segments:
         all_words.extend(segment['words'])
     
-    for i, word in enumerate(all_words):
+    # Process each word individually
+    for word in all_words:
         word_text = word['text']
+        start_time = word['start']
+        end_time = word['end']
         
-        # Check if this word would exceed maxCaptionSize
-        new_length = current_length + len(word_text) + (1 if current_caption else 0)
-        
-        # Determine if we should split here
-        should_split = (
-            new_length > maxCaptionSize or
-            (considerPunctuation and word_text.rstrip('.,!?') != word_text and current_caption) or
-            i == len(all_words) - 1 or
-            len(current_caption) >= 5
-        )
-        
-        # Add word to current caption if we're not splitting yet
-        if not should_split:
-            current_caption.append(word_text)
-            current_length = new_length
-            continue
-            
-        # Handle the split
-        if current_caption:
-            # Add current word if this is the last one
-            if i == len(all_words) - 1 and new_length <= maxCaptionSize:
-                current_caption.append(word_text)
-                
-            caption_text = ' '.join(current_caption)
-            start_time = all_words[i - len(current_caption)]['start']
-            end_time = word['end'] if word_text in current_caption else all_words[i - 1]['end']
-            time_splits.append(((start_time, end_time), caption_text))
-            
-        # Handle current word if it wasn't added to the previous caption
-        if word_text not in current_caption and i == len(all_words) - 1:
-            time_splits.append(((word['start'], word['end']), word_text))
-            
-        # Reset for next caption
-        current_caption = []
-        current_length = 0
-        
-        # Start new caption with current word if it wasn't the last one
-        if i < len(all_words) - 1:
-            current_caption.append(word_text)
-            current_length = len(word_text)
+        # Store each word with its exact timing
+        time_splits.append(((start_time, end_time), word_text))
     
     return time_splits
